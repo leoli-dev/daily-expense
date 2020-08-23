93c8839f4272a4f6d5e37a70edb9c23a3d582e67
Xiaojie LI <leo.li.ca@outlook.com>
2020-08-23 12:10:16 -0400

    remove useless libraries

--- config/bundles.php
+++ config/bundles.php
@@ -13,8 +13,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    BM\BackupManagerBundle\BMBackupManagerBundle::class => ['all' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
